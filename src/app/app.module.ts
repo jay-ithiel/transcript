af9d34e8b92a2250ac056e7c48bc5e6e46911484
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-30 13:28:47 -0800

    Add Searchmodule & routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FooterModule } from './Footer/Footer.module';
+import { SearchComponent } from './Search/Search.component';
+import { SearchModule } from './Search/Search.module';
+
+const appRoutes: Routes = [
+  { path: '', component: SearchComponent },
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     FooterModule,
+    SearchModule,
+    RouterModule.forRoot(appRoutes),
   ],
   declarations: [AppComponent],
   providers: [],
