6063a924bc7b29dcb39bc88ac8870c54721b728f
Jay Ithiel <jay.ithiel@gmail.com>
2018-12-02 14:56:03 -0800

    Create notfound 404 component

---
+++ src/app/NotFound/NotFound.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './NotFound.component.html',
+  styleUrls: ['./NotFound.component.scss'],
+})
+export class NotFoundComponent { }
